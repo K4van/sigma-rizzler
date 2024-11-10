e37824c59855cff29d379cfc7bdd81c3dbf0e9b3
Dirga Yuza <dirgayuza123@gmail.com>
2024-11-11 01:24:59 +0700

    sigma_rizzler.c: Move functions to a separate header file

---
+++ sigma.h
@@ -0,0 +1,25 @@
+#ifndef SIGMA_H
+#define SIGMA_H
+#include <stdio.h>
+
+void printAuthentication()
+{
+    
+}
+
+void authRegister()
+{
+
+}
+void printLogo()
+{
+    const char* LOGO = "________  ______  ________  ___        ___       ___      \n"
+                       "__          __    __        ___ __  __ ___     __   __    \n"
+                       "________    __    __   ___  ___  _  _  ___    __ ___ __   \n"
+                       "      __    __    __    __  ___   __   ___   __       __  \n"
+                       "________  ______  ________  ___        ___  __         __ \n";
+
+    printf(LOGO);
+}
+
+#endif
--- sigma_rizzler.c
+++ sigma_rizzler.c
@@ -3,33 +3,12 @@
  * 
  * A simple turn-based game about Sigma and Rizz
 */
+
 #include <stdio.h>
-
-void printAuthentication()
-{
-    
-}
-
-void authRegister()
-{
-
-}
-void printLogo()
-{
-    const char* LOGO = "________  ______  ________  ___        ___       ___      \n"
-                       "__          __    __        ___ __  __ ___     __   __    \n"
-                       "________    __    __   ___  ___  _  _  ___    __ ___ __   \n"
-                       "      __    __    __    __  ___   __   ___   __       __  \n"
-                       "________  ______  ________  ___        ___  __         __ \n";
-
-    printf(LOGO);
-}
+#include "sigma.h"
 
 int main()
 {   
     printLogo();
-        // do{
-
-        // }while();
     return 0;
 }
